795504ed11d3d19239e40c49e81e051172971fc9
Malavika <malavika.selvan@guhatek.com>
2025-04-16 09:05:22 +0000

    new change

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello from Malavika! Welcome to Guhatek'));
+app.get('/', (req, res) => res.send('Hello all ! Welcome to Guhatek'));
 
 app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
